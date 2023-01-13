ce0b70b4b4d8b2ef66f0257049a01867f3404889
genesix <49217079+genesix@users.noreply.github.com>
2023-01-13 04:48:50 +0100

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: Collins Chukwuebuka Anibuko
    # Additional Authors:
-   # <your GitHub username>
+   # genesix
 
    # Input:
    # p, principal amount
